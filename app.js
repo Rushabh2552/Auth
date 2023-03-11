30d33c421564dfa4196fed7b12adee83bc767b74
Rushabh Modi <rushabhmodi2552@gmail.com>
2023-03-11 18:05:27 +0530

    submit secret#1

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -95,6 +96,33 @@ app.get('/secrets', function(req, res){
     }
 });
 
+
+app.get('/submit', function(req, res){
+    if (req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect('/login'); 
+    }
+})
+
+app.post('/submit', function(req, res){
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect('/secrets');
+                });
+            }
+        }
+    });
+}); 
 
 app.get('/logout', function(req, res){
     req.logout(function(err) {
