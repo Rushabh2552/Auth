1223f5a0f647cdff3072069cdc4dda71e9a020eb
Rushabh Modi <rushabhmodi2552@gmail.com>
2023-03-11 18:23:10 +0530

    render Secrets

--- app.js
+++ app.js
@@ -89,11 +89,16 @@ app.get('/register', function(req, res) {
 })
 
 app.get('/secrets', function(req, res){
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect('/login'); 
-    }
+    
+    User.find({secret: {$ne:null}}, function(err, foundUsers){
+        if (err) {
+            console.log(err);
+        } else {
+           if (foundUsers) {
+            res.render("secrets", {usersWithSecrets: foundUsers})
+           }
+        }
+    })
 });
 
 
@@ -107,8 +112,6 @@ app.get('/submit', function(req, res){
 
 app.post('/submit', function(req, res){
     const submittedSecret = req.body.secret;
-
-    console.log(req.user.id);
 
     User.findById(req.user.id, function(err, foundUser){
         if (err) {
